6edbf7623ec6249588cdcd043ad118d3ed3da8ec
Matheus Castello <matheus.castello@toradex.com>
2020-09-15 21:03:36 -0300

    moses: generateclients: Format and add typescript in powershell script
    Using multi-line commands improves readability. Also adding the
    generation of files for typescript in the script for powershell.
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- moses/generateclients.ps1
+++ moses/generateclients.ps1
@@ -1,4 +1,33 @@
 #!/bin/sh
 
-docker run -v ${PWD}/..:/local openapitools/openapi-generator-cli generate -i /local/moses/swagger.yaml -g csharp -o /local/clients/csharp/TorizonAppDeploymentAPI/generated --additional-properties packageName=TorizonRestAPI --additional-properties targetFramework=v4.5
-docker run -v ${PWD}/..:/local openapitools/openapi-generator-cli generate -i /local/moses/swagger.yaml -g python -o /local/clients/python --additional-properties packageName=moses_client
+# csharp
+docker run `
+    -v ${PWD}/..:/local `
+    openapitools/openapi-generator-cli `
+    generate `
+    -i /local/moses/swagger.yaml `
+    -g csharp `
+    -o /local/clients/csharp/TorizonAppDeploymentAPI/generated `
+    --additional-properties packageName=TorizonRestAPI `
+    --additional-properties targetFramework=v4.5
+
+# python
+docker run `
+    -v ${PWD}/..:/local `
+    openapitools/openapi-generator-cli `
+    generate `
+    -i /local/moses/swagger.yaml `
+    -g python `
+    -o /local/clients/python `
+    --additional-properties packageName=moses_client
+
+# typescript
+docker run `
+    -v ${PWD}/..:/local `
+    openapitools/openapi-generator-cli `
+    generate `
+    -i /local/moses/swagger.yaml `
+    -g typescript-node `
+    -o /local/clients/typescript `
+    --additional-properties npmName=torizonrestapi `
+    --additional-properties supportsES6=true
--- moses/generateclients.sh
+++ moses/generateclients.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
-docker run -v $(pwd)/..:/local --user $(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/moses/swagger.yaml -g csharp -o /local/clients/csharp/TorizonAppDeploymentAPI/generated --additional-properties packageName=TorizonRestAPI --additional-properties targetFramework=v4.5
-docker run -v $(pwd)/..:/local --user $(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/moses/swagger.yaml -g python -o /local/clients/python --additional-properties packageName=moses_client
-docker run -v $(pwd)/..:/local --user $(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/moses/swagger.yaml -g typescript-node -o /local/clients/typescript --additional-properties npmName=torizonrestapi --additional-properties supportsES6=true
+# csharp
+docker run \
+    -v $(pwd)/..:/local \
+    --user $(id -u):$(id -g) \
+    openapitools/openapi-generator-cli \
+    generate \
+    -i /local/moses/swagger.yaml \
+    -g csharp \
+    -o /local/clients/csharp/TorizonAppDeploymentAPI/generated \
+    --additional-properties packageName=TorizonRestAPI \
+    --additional-properties targetFramework=v4.5
+
+#  python
+docker run \
+    -v $(pwd)/..:/local \
+    --user $(id -u):$(id -g) \
+    openapitools/openapi-generator-cli \
+    generate \
+    -i /local/moses/swagger.yaml \
+    -g python \
+    -o /local/clients/python \
+    --additional-properties packageName=moses_client
+
+# typescript
+docker run \
+    -v $(pwd)/..:/local \
+    --user $(id -u):$(id -g) \
+    openapitools/openapi-generator-cli \
+    generate \
+    -i /local/moses/swagger.yaml \
+    -g typescript-node \
+    -o /local/clients/typescript \
+    --additional-properties npmName=torizonrestapi \
+    --additional-properties supportsES6=true
