92da9192272fa6539b7be9aaefdc1c195308b8b8
Valter Minute <valter.minute@toradex.com>
2021-08-16 14:02:41 +0200

    fix property setting in test_setup.sh
    Related-to: TIE-480
    Signed-off-by: Valter Minute <valter.minute@toradex.com>

--- moses/platforms/arm64v8-qt5-vivante-no-ssh_bullseye/testfiles/test_setup.sh
+++ moses/platforms/arm64v8-qt5-vivante-no-ssh_bullseye/testfiles/test_setup.sh
@@ -2,4 +2,4 @@
 
 application=$($TDSKT load $MOSES_CURRENT_APPLICATION_FOLDER)
 
-$TDSKT application $application setprop appname test common
+$TDSKT application $application setprop props.appname test common
