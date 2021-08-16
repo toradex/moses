fc0dcad05653b15cac4df4090c39fbae700dc466
Valter Minute <valter.minute@toradex.com>
2021-08-16 14:02:41 +0200

    fix property setting in test_setup.sh
    Related-to: TIE-480
    Signed-off-by: Valter Minute <valter.minute@toradex.com>

--- moses/platforms/arm64v8-debian-python3-qt-vivante_buster/testfiles/test_setup.sh
+++ moses/platforms/arm64v8-debian-python3-qt-vivante_buster/testfiles/test_setup.sh
@@ -2,5 +2,5 @@
 
 application=$($TDSKT load $MOSES_CURRENT_APPLICATION_FOLDER)
 
-$TDSKT application $application setprop appname test common
-$TDSKT application $application setprop main main.py common
+$TDSKT application $application setprop props.appname test common
+$TDSKT application $application setprop props.main main.py common
