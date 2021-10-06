e7666983d41bc88684d22b5c8a2a31c028e8a076
Valter Minute <valter.minute@toradex.com>
2021-10-06 16:17:18 +0200

    add per-platform validation scripts
    Signed-off-by: Valter Minute <valter.minute@toradex.com>

---
+++ moses/platforms/arm32v7-dotnet-uno_bullseye/validate.py
@@ -0,0 +1,9 @@
+"""Platform-specific validation."""
+import validation
+import validation_utils
+
+def init_app_tables(
+    application: validation.ExtendedValidation) -> None:
+    """Add platform-specific validation to application object."""
+    validation_utils.add_base_container_props_validation(application)
+    validation_utils.add_debian_container_props_validation(application)
