toradex/moses

commit 7c688a620eed9d856774204cc64d643fecc82949

add validation support
Signed-off-by: Valter Minute <valter.minute@toradex.com>

moses/applicationconfig_deployrun.py
@@ -627,7 +627,9 @@ def run(self: ApplicationConfigBase,
 
         ports = self._merge_props(plat, configuration, "ports")
         volumes = self._merge_props(plat, configuration, "volumes")
-        devices = self._append_props(plat, configuration, "devices")
+        devices = list(
+            dict.fromkeys(self._append_props(plat, configuration, "networks"))
+        )
         extraparms = self._merge_props(plat, configuration, "extraparms")
         networks = list(
             dict.fromkeys(self._append_props(plat, configuration, "networks"))

cli/tdskt.py
@@ -761,6 +761,27 @@ def cmd_handler_device_ip(args) -> int:
     logging.info(ip)
     return 0
 
+def cmd_handler_device_validate(args) -> int:
+    """Sync a local folder with a folder on the device
+
+    Arguments:
+        args -- parsed command line arguments
+
+    Returns:
+        int -- 0 for success
+    """
+    api = moses_client.api.devices_api.DevicesApi()
+    result = api.device_validate_parameter(args.device_id,args.parameter,args.value)
+    retcode = 0
+
+    for warning in result.warnings:
+        logging.warning(warning)
+        retcode = 2
+    for error in result.errors:
+        logging.error(error)
+        retcode = 1
+    return retcode
+
 
 def cmd_handler_application_info(args) -> int:
     """dumps application info
@@ -1060,7 +1081,7 @@ def cmd_handler_application_setprop(args) -> int:
         application.props[args.configuration][propertyname]="" if value is None else value
     else:
         if args.configuration is None:
-            setattr(application,args.property,value)
+            setattr(application,args.property,str(value))
         else:
             dictionary=getattr(application,args.property)
             dictionary[args.configuration]=value
@@ -1087,6 +1108,51 @@ def cmd_handler_application_publish(args) -> int:
         args.application_id, args.credentials, args.username, args.password, progress_id=progress_id
     )
     return 0
+
+def cmd_handler_application_validate_parameter(args) -> int:
+    """Validate a simple parameter"""
+    api = moses_client.api.applications_api.ApplicationsApi()
+
+    result = api.application_validate_parameter(args.application_id, args.configuration, args.parameter, args.value)
+    retcode = 0
+
+    for warning in result.warnings:
+        logging.warning(warning)
+        retcode = 2
+    for error in result.errors:
+        logging.error(error)
+        retcode = 1
+    return retcode
+
+def cmd_handler_application_validate_item(args) -> int:
+    """Validate an array item"""
+    api = moses_client.api.applications_api.ApplicationsApi()
+
+    result = api.application_validate_array_item(args.application_id, args.configuration, args.array, args.value, args.index)
+    retcode = 0
+
+    for warning in result.warnings:
+        logging.warning(warning)
+        retcode = 2
+    for error in result.errors:
+        logging.error(error)
+        retcode = 1
+    return retcode
+
+def cmd_handler_application_validate_entry(args) -> int:
+    """Validate an array item"""
+    api = moses_client.api.applications_api.ApplicationsApi()
+
+    result = api.application_validate_dictionary_entry(args.application_id, args.configuration, args.dictionary, args.key, args.value, args.newitem)
+    retcode = 0
+
+    for warning in result.warnings:
+        logging.warning(warning)
+        retcode = 2
+    for error in result.errors:
+        logging.error(error)
+        retcode = 1
+    return retcode
 
 def cmd_handler_detect(args) -> int:
     """Detects a new serial or network device
@@ -1352,6 +1418,7 @@ def create_parser() -> argparse.ArgumentParser:
     device_subparsers.add_parser("key")
     device_sync_parser = device_subparsers.add_parser("sync")
     device_subparsers.add_parser("ip")
+    device_validate_parser = device_subparsers.add_parser("validate")
 
     # add sub-commands for device image
     device_image_parser.add_argument(
@@ -1387,6 +1454,14 @@ def create_parser() -> argparse.ArgumentParser:
         help="Destination folder (target device)",
         metavar="destination-folder",
     )
+
+    device_validate_parser.add_argument(
+        "parameter",
+        help="Parameter being validated")
+
+    device_validate_parser.add_argument(
+        "value",
+        help="Value for the parameter being validated")
 
     detect_parser.add_argument(
         "target", type=str, help="serial port or ip address/hostname"
@@ -1438,6 +1513,7 @@ def create_parser() -> argparse.ArgumentParser:
     application_setprop_parser = application_subparsers.add_parser("setprop")
     application_logs_parser = application_subparsers.add_parser("logs")
     application_publish_parser = application_subparsers.add_parser("publish")
+    application_validate_parser = application_subparsers.add_parser("validate")
 
     application_build_parser.add_argument(
         "configuration", help="debug/release or other app-specific configuration"
@@ -1537,6 +1613,7 @@ def create_parser() -> argparse.ArgumentParser:
     application_logs_parser.add_argument(
         "configuration", help="debug/release or other app-specific configuration"
     )
+
     application_logs_parser.add_argument(
         "device_id", help="Device serial number", metavar="device-id"
     )
@@ -1550,6 +1627,53 @@ def create_parser() -> argparse.ArgumentParser:
     )
     application_publish_parser.add_argument(
         "password", help="password/token used for authentication"
+    )
+
+    application_validate_subparsers = application_validate_parser.add_subparsers(dest="subsubcommand")
+
+    application_validate_parameter_parser = application_validate_subparsers.add_parser("parameter")
+    application_validate_item_parser = application_validate_subparsers.add_parser("item")
+    application_validate_entry_parser = application_validate_subparsers.add_parser("entry")
+
+    application_validate_parameter_parser.add_argument(
+        "configuration", help="debug/release/common"
+    )
+    application_validate_parameter_parser.add_argument(
+        "parameter", help="parameter name"
+    )
+    application_validate_parameter_parser.add_argument(
+        "value", help="parameter value"
+    )
+
+    application_validate_item_parser.add_argument(
+        "configuration", help="debug/release/common"
+    )
+    application_validate_item_parser.add_argument(
+        "array", help="array name"
+    )
+    application_validate_item_parser.add_argument(
+        "value", help="item value"
+    )
+
+    application_validate_item_parser.add_argument(
+        "index", help="item index (-1 for new item)",
+        type=int, nargs="?", default=-1
+    )
+    application_validate_entry_parser.add_argument(
+        "configuration", help="debug/release/common"
+    )
+    application_validate_entry_parser.add_argument(
+        "dictionary", help="dictionary name"
+    )
+    application_validate_entry_parser.add_argument(
+        "key", help="key value"
+    )
+    application_validate_entry_parser.add_argument(
+        "value", help="entry value"
+    )
+    application_validate_entry_parser.add_argument(
+        "--newitem", help="set if item is going to be added to dict",
+        action="store_true", default=False
     )
 
     eula_parser.add_argument(

moses/.mypy.ini
@@ -48,3 +48,6 @@ ignore_missing_imports = True
 
 [mypy-OpenSSL]
 ignore_missing_imports = True
+
+[mypy-jsonschema]
+ignore_missing_imports = True

moses/api.py
@@ -29,6 +29,8 @@ EMULATION_IMAGE_NAME = "torizon/binfmt"
 
 # validation of readonly fields is done internally
 # pylint: disable=unused-argument
+
+
 def remove_readonly(validator: Any, readonly: Any, instance: Any,
                     schema: Any) -> None:
     """Skip validation of readonly fields.
@@ -130,11 +132,13 @@ def version_docker_get() -> Any:
     try:
         client = docker.from_env()
 
-        return (openapienforce.normalize_object_from_type(client.version(),"Docker_Version"), 200)
+        return (openapienforce.normalize_object_from_type(
+            client.version(), "Docker_Version"), 200)
     # pylint: disable=broad-except
     except Exception as exception:
         logging.exception("docker version")
         raise LocalDockerError(exception) from exception
+
 
 def devices_get() -> Any:
     """Return the list of devices.
@@ -401,7 +405,7 @@ def devices_device_processes_get(device_id: str) -> Any:
 
     processes = devices[device_id].get_process_list()
     processes.sort(key=lambda x: x["pid"])
-    return (openapienforce.normalize_array_from_type(processes,"Process"), 200)
+    return (openapienforce.normalize_array_from_type(processes, "Process"), 200)
 
 
 def devices_device_memory_get(device_id: str) -> Any:
@@ -439,7 +443,8 @@ def devices_device_storage_get(device_id: str) -> Any:
 
     mountpoints = devices[device_id].get_storageinfo()
     mountpoints.sort(key=lambda x: x["mountpoint"])
-    return (openapienforce.normalize_array_from_type(mountpoints,"MountPoint"), 200)
+    return (openapienforce.normalize_array_from_type(
+        mountpoints, "MountPoint"), 200)
 
 
 def devices_device_images_get(device_id: str) -> Any:
@@ -462,7 +467,8 @@ def devices_device_images_get(device_id: str) -> Any:
         if ("RepoTags" in x.keys() and len(x["RepoTags"]) > 0)
         else "~" + x["Id"]
     )
-    return (openapienforce.normalize_array_from_type(images,"Docker_Image"), 200)
+    return (openapienforce.normalize_array_from_type(
+        images, "Docker_Image"), 200)
 
 
 def devices_device_images_image_get(device_id: str, image_id: str) -> Any:
@@ -485,7 +491,8 @@ def devices_device_images_image_get(device_id: str, image_id: str) -> Any:
 
     if image is None:
         return ("Image not found", 404)
-    return (openapienforce.normalize_object_from_type(image.attrs,"Docker_Image"), 200)
+    return (openapienforce.normalize_object_from_type(
+        image.attrs, "Docker_Image"), 200)
 
 
 def devices_device_images_image_delete(device_id: str, image_id: str) -> Any:
@@ -529,7 +536,8 @@ def devices_device_containers_get(device_id: str) -> Any:
         if ("Name" in x.keys() and x["Name"] is not None)
         else "~" + x["Id"]
     )
-    return (openapienforce.normalize_array_from_type(containers,"Docker_Container"), 200)
+    return (openapienforce.normalize_array_from_type(
+        containers, "Docker_Container"), 200)
 
 
 def devices_device_containers_container_get(
@@ -553,7 +561,8 @@ def devices_device_containers_container_get(
 
     if container is None:
         return ("Container not found", 404)
-    return (openapienforce.normalize_object_from_type(container.attrs,"Docker_Container"), 200)
+    return (openapienforce.normalize_object_from_type(
+        container.attrs, "Docker_Container"), 200)
 
 
 def devices_device_containers_container_delete(
@@ -597,7 +606,8 @@ def devices_device_containers_container_start_get(
 
     device = devices[device_id]
     container = device.start_container(container_id)
-    return (openapienforce.normalize_object_from_type(container.attrs,"Docker_Container"), 200)
+    return (openapienforce.normalize_object_from_type(
+        container.attrs, "Docker_Container"), 200)
 
 
 def devices_device_containers_container_stop_get(
@@ -619,7 +629,8 @@ def devices_device_containers_container_stop_get(
 
     device = devices[device_id]
     container = device.stop_container(container_id)
-    return (openapienforce.normalize_object_from_type(container.attrs,"Docker_Container"), 200)
+    return (openapienforce.normalize_object_from_type(
+        container.attrs, "Docker_Container"), 200)
 
 
 def devices_device_containers_container_processes_get(
@@ -641,7 +652,7 @@ def devices_device_containers_container_processes_get(
 
     processes = devices[device_id].get_container_process_list(container_id)
     processes.sort(key=lambda x: x["pid"])
-    return (openapienforce.normalize_array_from_type(processes,"Process"), 200)
+    return (openapienforce.normalize_array_from_type(processes, "Process"), 200)
 
 
 def devices_device_containers_container_memory_get(
@@ -662,8 +673,8 @@ def devices_device_containers_container_memory_get(
         return ("Device not found", 404)
 
     return (openapienforce.normalize_object_from_type(
-                devices[device_id].get_container_memoryinfo(container_id),
-                "MemInfo"), 200)
+        devices[device_id].get_container_memoryinfo(container_id),
+        "MemInfo"), 200)
 
 
 def devices_device_containers_container_storage_get(
@@ -685,7 +696,8 @@ def devices_device_containers_container_storage_get(
 
     mountpoints = devices[device_id].get_container_storageinfo(container_id)
     mountpoints.sort(key=lambda x: x["mountpoint"])
-    return (openapienforce.normalize_array_from_type(mountpoints,"MountPoint"), 200)
+    return (openapienforce.normalize_array_from_type(
+        mountpoints, "MountPoint"), 200)
 
 
 def devices_device_containers_container_logs_get(
@@ -784,6 +796,28 @@ def devices_device_current_ip_get(device_id: str) -> Any:
         return ("Device not found", 404)
 
     return (devices[device_id].get_current_ip(), 200)
+
+
+def devices_device_validate_parameter_get(
+        device_id: str, parameter: str, value: str) -> Any:
+    """Validate a parameter for a device.
+
+    :param device_id: device id
+    :type device_id: str
+    :param parameter: parameter to validate
+    :type parameter: str
+    :param value: parameter value
+    :type value: str
+
+    :returns: API tuple with object and return code
+
+    """
+    devices = targetdevice.TargetDevices()
+
+    if device_id not in devices:
+        return ("Device not found", 404)
+
+    return (devices[device_id].validate(parameter, value), 200)
 
 
 def eulas_get() -> Any:
@@ -1146,7 +1180,8 @@ def applications_application_run_get(
 
         progresscookie.progress_completed(progress)
 
-        return (openapienforce.normalize_object_from_type(container,"Docker_Container"), 200)
+        return (openapienforce.normalize_object_from_type(
+            container, "Docker_Container"), 200)
     except Exception as exception:
         progresscookie.progress_report_error(progress, exception)
         raise
@@ -1213,7 +1248,8 @@ def applications_application_container_get(
     if container is None:
         return ("Container not found", 404)
 
-    return (openapienforce.normalize_object_from_type(container.attrs,"Docker_Container"), 200)
+    return (openapienforce.normalize_object_from_type(
+        container.attrs, "Docker_Container"), 200)
 
 
 def applications_application_sdk_container_get(
@@ -1239,7 +1275,8 @@ def applications_application_sdk_container_get(
     if container is None:
         return (connexion.NoContent, 204)
 
-    return (openapienforce.normalize_object_from_type(container.attrs,"Docker_Container"), 200)
+    return (openapienforce.normalize_object_from_type(
+        container.attrs, "Docker_Container"), 200)
 
 
 def applications_application_container_logs_get(
@@ -1413,7 +1450,7 @@ def applications_application_sdk_run_get(
         progresscookie.progress_completed(progress)
 
         if app.sdksshaddress[configuration] is None:
-            return ({"HostIp":"", "HostPort": ""}, 200)
+            return ({"HostIp": "", "HostPort": ""}, 200)
         return (app.sdksshaddress[configuration], 200)
     except Exception as exception:
         progresscookie.progress_report_error(progress, exception)
@@ -1555,6 +1592,7 @@ def applications_application_reseal_get(application_id: str) -> Any:
     app.reseal()
     return (connexion.NoContent, 200)
 
+
 def applications_application_publish_get(
         application_id: str, credentials: str,
         dockeruser: str, dockerpass: str, progress_id: str = None) -> Any:
@@ -1599,10 +1637,111 @@ def applications_application_publish_get(
         raise
 
 
+def applications_application_validate_parameter_get(
+        application_id: str,
+        configuration: str,
+        parameter: str,
+        value: str) -> Any:
+    """Validate a parameter for a device.
+
+    :param application_id: application id
+    :type application_id: str
+    :param configuration: debug/release
+    :type configuration: str
+    :param parameter: parameter to validate
+    :type parameter: str
+    :param value: parameter value
+    :type value: str
+
+    :returns: API tuple with object and return code
+
+    """
+    applications = applicationconfig.ApplicationConfigs()
+
+    if application_id not in applications:
+        raise moses_exceptions.ObjectNotFound(
+            "Application", application_id)
+
+    app = applications[application_id]
+
+    result = app.validate_configuration_parameter(
+        configuration, parameter, value)
+    return (result, 200)
+
+
+def applications_application_validate_array_item_get(
+        application_id: str,
+        configuration: str,
+        parameter: str,
+        value: str,
+        index: int) -> Any:
+    """Validate an array parameter for an application.
+
+    :param application_id: application id
+    :type application_id: str
+    :param configuration: debug/release
+    :type configuration: str
+    :param parameter: parameter to validate
+    :type parameter: str
+    :param value: item value
+    :type value: str
+
+    :returns: API tuple with object and return code
+
+    """
+    applications = applicationconfig.ApplicationConfigs()
+
+    if application_id not in applications:
+        raise moses_exceptions.ObjectNotFound(
+            "Application", application_id)
+
+    app = applications[application_id]
+
+    result = app.validate_configuration_array_item(
+        configuration, parameter, value, index)
+    return (result, 200)
+
+
+def applications_application_validate_dictionary_entry_get(
+        application_id: str,
+        configuration: str,
+        parameter: str,
+        key: str,
+        value: str,
+        newitem: bool) -> Any:
+    """Validate an array parameter for an application.
+
+    :param application_id: application id
+    :type application_id: str
+    :param configuration: debug/release
+    :type configuration: str
+    :param parameter: parameter to validate
+    :type parameter: str
+    :param key: entry key
+    :type key: str
+    :param value: entry value
+    :type value: str
+
+    :returns: API tuple with object and return code
+
+    """
+    applications = applicationconfig.ApplicationConfigs()
+
+    if application_id not in applications:
+        raise moses_exceptions.ObjectNotFound(
+            "Application", application_id)
+
+    app = applications[application_id]
+
+    result = app.validate_configuration_dictionary_entry(
+        configuration, parameter, key, value, newitem)
+    return (result, 200)
 
 # pylint: disable=too-many-nested-blocks
 # pylint: disable=too-many-branches
 # pylint: disable=too-many-statements
+
+
 def setup_pullcontainers_get(progress_id: str = None) -> Any:
     """Pull all base containers needed for the different applications.
 
@@ -1758,7 +1897,8 @@ def progress_create_get() -> Any:
 
     progress_data = cookies.get_update(cookie.id, False)
 
-    return (openapienforce.normalize_object_from_type(progress_data,"Progress"), 200)
+    return (openapienforce.normalize_object_from_type(
+        progress_data, "Progress"), 200)
 
 
 def progress_delete_get(progress_id: str = "") -> Any:
@@ -1792,10 +1932,11 @@ def progress_status_get(progress_id: str = "") -> Any:
     if progress_data is None:
         return (connexion.NoContent, 404)
 
-    return (openapienforce.normalize_object_from_type(progress_data,"Progress"), 200)
+    return (openapienforce.normalize_object_from_type(
+        progress_data, "Progress"), 200)
 
 
-def init_api(schema: Dict[str,Any]) -> None:
+def init_api(schema: Dict[str, Any]) -> None:
     """Initialize the api and allocates all required objects.
 
     :param schema: contents of swagger.YAML

moses/applicationconfig.py
@@ -19,6 +19,7 @@ import applicationconfig_deployrun
 import applicationconfig_sdk
 import applicationconfig_dockerexport
 import applicationconfig_publish
+import applicationconfig_validation
 
 
 class ApplicationConfig(ApplicationConfigBase):
@@ -42,6 +43,22 @@ class ApplicationConfig(ApplicationConfigBase):
     push_to_registry = applicationconfig_dockerexport.push_to_registry
 
     publish = applicationconfig_publish.publish
+
+    get_array = applicationconfig_validation.get_array
+    get_dictionary = applicationconfig_validation.get_dictionary
+
+    validate_configuration_parameter = applicationconfig_validation.validate_parameter
+    validate_configuration_array_item = applicationconfig_validation.validate_array_item
+    validate_configuration_dictionary_entry = applicationconfig_validation.validate_dictionary_entry
+
+    def __init__(self,folder: Optional[Path] = None, platformid: str = ""):
+        """Initialize the object and its base classes.
+
+        :param folder: folder
+        :type folder: Optional[Path], optional
+        """
+        ApplicationConfigBase.__init__(self, folder, platformid)
+        applicationconfig_validation.init(self)
 
     def touch(self) -> None:
         """Set modification date to current time."""
@@ -196,6 +213,8 @@ class ApplicationConfigs(Dict[str, ApplicationConfig],
         :rtype: ApplicationConfig
 
         """
+        assert platform.id is not None
+
         # finds a new folder for the app config
         counter = 0
 
@@ -205,11 +224,8 @@ class ApplicationConfigs(Dict[str, ApplicationConfig],
         while (rootfolder / ("appconfig_" + str(counter))).exists():
             counter += 1
 
-        app = ApplicationConfig(rootfolder / ("appconfig_" + str(counter)))
+        app = ApplicationConfig(rootfolder / ("appconfig_" + str(counter)), platform.id)
 
-        assert platform.id is not None
-
-        app.platformid = platform.id
         app.username = username
         app.save()
 

moses/applicationconfig_base.py
@@ -16,11 +16,12 @@ import config
 import properties
 import platformconfig
 import tags
-
+import validation
 
 # pylint: disable=too-many-instance-attributes
+# pylint: disable=abstract-method
 class ApplicationConfigBase(
-        config.ConfigurableKeysObject, properties.PropertiesObject):
+        config.ConfigurableKeysObject, properties.PropertiesObject, validation.ExtendedValidation):
     """Class used to manage an application."""
 
     readonlyfields = config.ConfigurableKeysObject.readonlyfields.union(
@@ -35,7 +36,8 @@ class ApplicationConfigBase(
         "shutdownscript"]
     configurations = ["common", "debug", "release"]
 
-    def __init__(self, folder: Optional[Path] = None):
+    # pylint: disable = super-init-not-called
+    def __init__(self, folder: Optional[Path] = None, platformid: str = ""):
         """Load data from the configuration folder.
 
         :param folder: path of the folder used to store application configuration
@@ -69,7 +71,7 @@ class ApplicationConfigBase(
 
         self.sdkimages: Dict[str, str] = {"debug": "", "release": ""}
 
-        self.platformid = ""
+        self.platformid = platformid
         self.publickey: Optional[str] = None
         self.privatekey: Optional[str] = None
         self.modificationdate = datetime.datetime.utcnow().isoformat()
@@ -197,6 +199,10 @@ class ApplicationConfigBase(
         fields["sdksshaddress"] = self.sdksshaddress
         if "logs" in fields:
             del fields["logs"]
+        del fields["dictionary_validation_table"]
+        del fields["array_validation_table"]
+        del fields["skip_table"]
+        del fields["validation_table"]
         return fields
 
     def to_json(self) -> Dict[str, Any]:

moses/moses.linux.spec
@@ -12,7 +12,7 @@ a = Analysis(['moses.py'],
                     ("licenses","licenses"),
                     ("../.pyenv/lib/python3.9/site-packages/openapi_spec_validator/resources/schemas","openapi_spec_validator/resources/schemas"),
                     ("../.pyenv/lib/python3.9/site-packages/swagger_ui_bundle/vendor/swagger-ui-3.30.0","api/ui")],
-             hiddenimports=[],
+             hiddenimports=[ "validation_utils" ],
              hookspath=[],
              runtime_hooks=[],
              excludes=[],

commit 22495868532717c91da656dbbfde16e670589204

applicationconfig_deployrun: Fix Devices configuration doesn't seem to work
regression from 7c688a6

Signed-off-by: Matheus Castello <matheus.castello@toradex.com>
Related-to: TIE-503

moses/applicationconfig_deployrun.py
@@ -628,7 +628,7 @@ def run(self: ApplicationConfigBase,
         ports = self._merge_props(plat, configuration, "ports")
         volumes = self._merge_props(plat, configuration, "volumes")
         devices = list(
-            dict.fromkeys(self._append_props(plat, configuration, "networks"))
+            dict.fromkeys(self._append_props(plat, configuration, "devices"))
         )
         extraparms = self._merge_props(plat, configuration, "extraparms")
         networks = list(