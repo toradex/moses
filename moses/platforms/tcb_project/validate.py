6cda9a59f84dd26dc01167cb9d1b12c4baa1567d
Matheus Castello <matheus.castello@toradex.com>
2021-11-24 09:41:51 -0300

    platforms: tcb: Add the TorizonCore Builder platform files
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>
    Related-to: TIE-245

---
+++ moses/platforms/tcb_project/validate.py
@@ -0,0 +1,9 @@
+"""Platform-specific validation."""
+import validation
+import validation_utils
+
+def init_app_tables(
+    application: validation.ExtendedValidation) -> None:
+    """Add platform-specific validation to application object."""
+    # nothing to add
+
